90247aa8fe4a6bf46d67c14a13dc02696f69b41f
stewartvon <stewartvon975@gmail.com>
2025-02-22 10:24:17 +0100

    update errors

--- src/errors/contracts.js
+++ src/errors/contracts.js
@@ -1,13 +1,13 @@
 export const CONTRACTS_ERRORS = {
   TRY_TO_CALL_NON_CALLABLE: new Error(
-    'The contract was created as non-callable but was attempted to be called!'
+      'The contract was created as non-callable, but an attempt was made to call it!'
   ),
   TRY_TO_CALL_READ_ONLY: new Error(
-    'The contract was created as read-only but was attempted to be called!'
+      'The contract was created as read-only, but an attempt was made to call it!'
   ),
-  ADDRESS_IS_NOT_PROVIDED: new Error('Address is not provided!'),
+  ADDRESS_IS_NOT_PROVIDED: new Error('A contract address was not provided!'),
   METHOD_NOT_FOUND: (methodName) =>
-    new Error(`Method ${methodName} not found on contract!`),
+      new Error(`Method "${methodName}" was not found on the contract!`),
   FRAGMENT_NOT_FOUND: (methodName) =>
-    new Error(`Fragment for method ${methodName} not found on contract!`),
+      new Error(`Fragment for method "${methodName}" was not found on the contract!`),
 };
